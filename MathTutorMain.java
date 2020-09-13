9177f11617448ae4852165dcebe8c5908ff6dd31
aytekinoldac99 <71228633+aytekinoldac99@users.noreply.github.com>
2020-09-13 14:05:31 -0400

    Create MathTutorMain.java

---
+++ MathTutorMain.java
@@ -0,0 +1,10 @@
+package GUI;
+
+public class MathTutorMain
+{
+    public static void main(String []args)
+    {
+        MathTutorGUI math = new MathTutorGUI();
+        math.setVisible(true);
+    }
+}
